a73c7c18138d69150e86fcf3eb60a4296588590b
Tyler Fisher <tfisher@politico.com>
2018-12-07 13:44:02 -0500

    add notes to financials

--- biography/models/financials.py
+++ biography/models/financials.py
@@ -3,15 +3,14 @@ from django.db import models
 
 class Financials(models.Model):
     biography = models.OneToOneField(
-        'Biography',
-        related_name="financials",
-        on_delete=models.CASCADE,
+        "Biography", related_name="financials", on_delete=models.CASCADE
     )
 
     net_worth = models.DecimalField(max_digits=14, decimal_places=2)
+    notes = models.TextField(null=True, blank=True)
 
     class Meta:
-        verbose_name_plural = 'financials'
+        verbose_name_plural = "financials"
 
     def __str__(self):
         return self.biography.person.full_name
